e9f43326ad8d45bf08bd398a42d8475154f0ad0a
Joakim Ekblad <aio.github@aio.nu>
2018-09-17 19:58:49 +0200

    Add another testcase

--- src/testcase_test.c
+++ src/testcase_test.c
@@ -1,18 +1,25 @@
+#include <stdlib.h>
 #include <stdio.h>
 
 #include "testcase.h"
-#include "/tmp/testcase_tarsio_data.h"
+#include "testcase_data.h"
 #include "tarsio.h"
 
 #define m tarsio_mock
 
 test(testcase_list_init_shall_call_parse_correctly) {
-  (void)__tarsio_mock_data;
-
   testcase_list_init((testcase_list_t*)0x1234, (file_t*)0x5678);
-  printf("Tjosan\n");
 
   assert_eq(1, m.parse.call_count);
   assert_eq((testcase_list_t*)0x1234, m.parse.args.arg0);
   assert_eq((file_t*)0x5678, m.parse.args.arg1);
+}
+
+test(parse_shall_mallocate_the_correct_number_of_bytes_plus_one_for_null) {
+  file_t file;
+  file.len = 5678;
+  parse((testcase_list_t*)0x1234, &file);
+
+  assert_eq(1, m.malloc.call_count);
+  assert_eq(5678 + 1, m.malloc.args.arg0);
 }
