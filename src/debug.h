094e2ac4821edb515c83acbad2c9ea04b59319a0
Joakim Ekblad <aio.github@aio.nu>
2018-09-17 10:18:39 +0200

    Made it possible to disable debug outputs as compile time

--- src/debug.h
+++ src/debug.h
@@ -3,16 +3,20 @@
 
 #include <stdio.h>
 
+#ifdef NODEBUG
 #define debug0(STR)
 #define debug1(FMT, ARG1)
 #define debug2(FMT, ARG1, ARG2)
 #define debug3(FMT, ARG1, ARG2, ARG3)
 #define debug4(FMT, ARG1, ARG2, ARG3, ARG4)
-/*
+#define debug5(FMT, ARG1, ARG2, ARG3, ARG4, ARG5)
+#else
 #define debug0(STR) fprintf(stderr, "DEBUG: " STR "\n")
 #define debug1(FMT, ARG1) fprintf(stderr, "DEBUG: " FMT "\n", ARG1)
 #define debug2(FMT, ARG1, ARG2) fprintf(stderr, "DEBUG: " FMT "\n", ARG1, ARG2)
 #define debug3(FMT, ARG1, ARG2, ARG3) fprintf(stderr, "DEBUG: " FMT "\n", ARG1, ARG2, ARG3)
 #define debug4(FMT, ARG1, ARG2, ARG3, ARG4) fprintf(stderr, "DEBUG: " FMT "\n", ARG1, ARG2, ARG3, ARG4)
-*/
+#define debug5(FMT, ARG1, ARG2, ARG3, ARG4, ARG5) fprintf(stderr, "DEBUG: " FMT "\n", ARG1, ARG2, ARG3, ARG4, ARG5)
+#endif
+
 #endif
