dd3f0161018a1a44f23c82b85ccdf32b1e30d203
Joakim Ekblad <aio.github@aio.nu>
2018-11-23 08:11:41 +0100

    Append generated documentatino for argument names

--- src/tsg.c
+++ src/tsg.c
@@ -126,9 +126,9 @@ static void generate_struct(prototype_list_t* list, cpp_list_t* cpp_list) {
       for (i = 0; i < anode->info.datatype.datatype_definition.is_pointer; i++) {
         printf("*");
       }
-      /*
-      printf(" %s", anode->info.name);
-       */
+
+      printf(" /* %s */", anode->info.name);
+
       if (NULL != anode->next) {
         printf(", ");
       }
