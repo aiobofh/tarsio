7dd468ba58d8d1633ca4821cb2bea000ebce3a76
Joakim Ekblad <aio.github@aio.nu>
2018-11-23 08:09:28 +0100

    Add more debug macros

--- src/debug.h
+++ src/debug.h
@@ -10,6 +10,8 @@
 #define debug3(FMT, ARG1, ARG2, ARG3)
 #define debug4(FMT, ARG1, ARG2, ARG3, ARG4)
 #define debug5(FMT, ARG1, ARG2, ARG3, ARG4, ARG5)
+#define debug6(FMT, ARG1, ARG2, ARG3, ARG4, ARG5, ARG6)
+#define debug7(FMT, ARG1, ARG2, ARG3, ARG4, ARG5, ARG6, ARG7)
 #else
 #define debug0(STR) fprintf(stderr, "DEBUG: " STR "\n")
 #define debug1(FMT, ARG1) fprintf(stderr, "DEBUG: " FMT "\n", ARG1)
@@ -17,6 +19,8 @@
 #define debug3(FMT, ARG1, ARG2, ARG3) fprintf(stderr, "DEBUG: " FMT "\n", ARG1, ARG2, ARG3)
 #define debug4(FMT, ARG1, ARG2, ARG3, ARG4) fprintf(stderr, "DEBUG: " FMT "\n", ARG1, ARG2, ARG3, ARG4)
 #define debug5(FMT, ARG1, ARG2, ARG3, ARG4, ARG5) fprintf(stderr, "DEBUG: " FMT "\n", ARG1, ARG2, ARG3, ARG4, ARG5)
+#define debug6(FMT, ARG1, ARG2, ARG3, ARG4, ARG5, ARG6) fprintf(stderr, "DEBUG: " FMT "\n", ARG1, ARG2, ARG3, ARG4, ARG5, ARG6)
+#define debug7(FMT, ARG1, ARG2, ARG3, ARG4, ARG5, ARG6, ARG7) fprintf(stderr, "DEBUG: " FMT "\n", ARG1, ARG2, ARG3, ARG4, ARG5, ARG6, ARG7)
 #endif
 
 #endif
