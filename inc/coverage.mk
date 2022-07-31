ee338ed49daf83c1823e595872ae1ed2c34bcd41
Joakim Ekblad <aio.github@aio.nu>
2022-07-31 14:36:33 +0200

    Remove _mocks.c and _runner.c from coverage

--- inc/coverage.mk
+++ inc/coverage.mk
@@ -31,7 +31,7 @@ LDFLAGS+=-fprofile-arcs -lgcov --coverage -fno-exceptions -fno-inline
 COVERAGE_XML:=${TTESTROOT}coverage.xml
 LINES_COV:=${TTMPROOT}lines.cov
 BRANCHES_COV:=${TTMPROOT}branches.cov
-FILTER=-e ".*${TCHECKSUFFIX}.c" -e ".*$tarsio.c"
+FILTER=-e ".*${TCHECKSUFFIX}.c" -e ".*$tarsio.c" -e ".*$\_mocks.c" -e ".*$\_runner.c"
 
 GCOVR:=$(shell which gcovr)
 
